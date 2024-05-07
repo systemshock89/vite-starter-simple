d7c175136727df7d2716db766675e889f6cbd533
systemshock89 <666qqq77>
2024-05-07 14:59:10 +0300

    add src folder

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import autoprefixer from 'autoprefixer';
 import viteImagemin from 'vite-plugin-imagemin'
 
 export default {
+    base: './',
+    root: "src",
+    build: {
+        outDir: '../dist',
+        emptyOutDir: true,
+    },
     server: {
         open: true,
     },
@@ -12,21 +18,20 @@ export default {
     },
     plugins: [
         viteImagemin({
-            // gifsicle: {
-            //     optimizationLevel: 7,
-            //     interlaced: false,
-            // },
-            // optipng: {
-            //     optimizationLevel: 7,
-            // },
-            // mozjpeg: {
-            //     quality: 75,
-            // },
-            // pngquant: {
-            //     quality: [0.8, 0.9],
-            //     speed: 4,
-            // },
-            mozjpeg: false,
+            gifsicle: {
+                optimizationLevel: 7,
+                interlaced: false,
+            },
+            optipng: {
+                optimizationLevel: 7,
+            },
+            mozjpeg: {
+                quality: 75,
+            },
+            pngquant: {
+                quality: [0.8, 0.9],
+                speed: 4,
+            },
             svgo: {
                 plugins: [
                     {
